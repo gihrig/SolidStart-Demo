2fbc0c040268cb6427e38f3889d818d471f96e5a
gihrig <glen@arkadias.com>
2025-11-30 01:35:19 -0800

    feat(app.css): Configure for dark and light theme responsive to system setting

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,12 @@ import Counter from "~/components/Counter";
 
 export default function Home() {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">Hello world!</h1>
+    <main class="text-center mx-auto text-(--theme-foreground) p-4">
+      <h1 class="max-6-xs text-6xl text-(--theme-accent) font-thin uppercase my-16">Hello SolidStart!</h1>
       <Counter />
       <p class="mt-8">
         Visit{" "}
-        <a href="https://solidjs.com" target="_blank" class="text-sky-600 hover:underline">
+        <a href="https://solidjs.com" target="_blank" class="text-(--theme-accent) hover:underline">
           solidjs.com
         </a>{" "}
         to learn how to build Solid apps.
@@ -16,7 +16,7 @@ export default function Home() {
       <p class="my-4">
         <span>Home</span>
         {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
+        <A href="/about" class="text-(--theme-accent) hover:underline">
           About Page
         </A>{" "}
       </p>
