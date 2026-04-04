418e260066a60c8e993d425b577ffa32fab35d7c
gihrig <glen@arkadias.com>
2026-04-03 17:14:00 -0700

    fix(src/entry-client.tsx): Eliminate build warning
    - [IMPORT_IS_UNDEFINED] Warning: Import `default` will always be undefined because there is no matching export in 'src/entry-client.tsx'

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,10 @@
 // @refresh reload
 import { mount, StartClient } from '@solidjs/start/client'
 
-mount(() => <StartClient />, document.getElementById('app')!)
+mount(
+  () => <StartClient />,
+  document.getElementById('app')!
+)
+
+// Only needed to avoid Rolldown build warning
+export default {}
