f5fca8b03cbd903b5126da9037e64fb71ac3fa06
gihrig <glen@arkadias.com>
2026-04-05 22:55:02 -0700

    feat(src/routes/[...404].tsx): Add links to 404 page
    Fixes:
    - error: Lint or type issues found
    - ⚠ eslint(no-unused-vars): Identifier 'A' is imported but never used.

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -7,6 +7,12 @@ export default function NotFound() {
       <Title>SolidStart 404</Title>
       <main>
         <h1>404 - Page Not Found</h1>
+        <br />
+        <P>Try one of these pages</P>
+        <A href="/">Home</A>
+        <A href="/about">About</A>
+        <A href="/readme">Readme</A>
+        <A href="/fullstack">FullStack</A>
       </main>
     </>
   )
