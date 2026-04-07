ba105a508fb4fae5ed2835d895ad4e815ca76a09
gihrig <glen@arkadias.com>
2026-04-06 21:20:02 -0700

    feat(src/routes/[...404].tsx): Revert added nav links - page navbar will suffice

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,5 +1,4 @@
 import { Title } from "@solidjs/meta";
-import { A } from "@solidjs/router";
 
 export default function NotFound() {
   return (
@@ -7,12 +6,6 @@ export default function NotFound() {
       <Title>SolidStart 404</Title>
       <main>
         <h1>404 - Page Not Found</h1>
-        <br />
-        <P>Try one of these pages</P>
-        <A href="/">Home</A>
-        <A href="/about">About</A>
-        <A href="/readme">Readme</A>
-        <A href="/fullstack">FullStack</A>
       </main>
     </>
   );
