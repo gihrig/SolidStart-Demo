415197c2584c39fbe370a8e4cb254258812aa776
gihrig <glen@arkadias.com>
2026-04-12 16:04:18 -0700

    dev(.vite-hooks/pre-commit): Enable check before commit hook

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -5,7 +5,7 @@ export default function Counter() {
   return (
     <button
       type="button"
-      class="dark: w-[200px] rounded-full border-2 border-gray-400 bg-gray-800 px-8 py-4 text-stone-100 focus:border-white active:border-gray-400 dark:bg-zinc-700 dark:text-stone-300 dark:focus:border-gray-300"
+      class="dark: w-50 rounded-full border-2 border-gray-400 bg-gray-800 px-8 py-4 text-stone-100 focus:border-white active:border-gray-400 dark:bg-zinc-700 dark:text-stone-300 dark:focus:border-gray-300"
       onClick={() => setCount(count() + 1)}
     >
       Clicks: {count()}
